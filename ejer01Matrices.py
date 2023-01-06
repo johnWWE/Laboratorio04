0b3b8b202b535cf3f9d2cdd133ae1baaf00df6b6
John Praddo <jhoncitof1ea10@gmail.com>
2023-01-06 11:25:08 -0500

    agregado la resta de matrices

--- ejer01Matrices.py
+++ ejer01Matrices.py
@@ -17,13 +17,15 @@ if filaA == filaB and columnaA ==columnaB:
     #matrizA
     for i in range(0,filaA):
         for j in range(0,columnaA):
-            matrizA[i][j]=int(input("Introduce el Elemento A["+str(i+1)+","+str(j+1)+"] =>"))
+            matrizA[i][j]=int(input("Introduce el Elemento A["+str(i+1)+","+str(j+1)+"] => "))
     print(f"matriz A: \n{matrizA}")
-    #matrizB
-    for i in range(0,filaA):
+    
+    #matrizB     
+    for i in range(0,filaA):    #ES implicito si es fila B o A ya que ambos deben ser del mimo orden para sumar y restar
         for j in range(0,columnaA):
-            matrizB[i][j]=int(input("Introduce el Elemento B["+str(i+1)+","+str(j+1)+"]=>"))
+            matrizB[i][j]=int(input("Introduce el Elemento B["+str(i+1)+","+str(j+1)+"]=> "))
     print(f"matriz B: \n{matrizB}")
+
 #operacion
     for i in range(0,filaA):
         for j in range(0,columnaA):
@@ -33,6 +35,16 @@ if filaA == filaB and columnaA ==columnaB:
 else:
     print("las Matrices A y B no se pueden sumar")
 
+#resta de matrices
+if filaA == filaB and columnaA ==columnaB:
+    for i in range(0,filaA):
+        for j in range(0,columnaA):
+            #for k in range(0,filaA):
+            matrizResult[i][j] = matrizA[i][j] - matrizB[i][j]
+    print(f".....matriz resultado A - B.....: \n{matrizResult}")
+else:
+    print("las Matrices A y B no se pueden restar porque  el orden es diferente")
+
 #producto
 ''' Evidentemente para el producto de matrices existen ciertas condiciones mas especificas
-que en una suma o resta de matrices'''
+que en una suma o resta de matrices, ya lo haremos paso por paso en ele ejercicio 02 de matrices'''
