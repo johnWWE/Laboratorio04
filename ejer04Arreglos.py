1c8598176f40cfddf48182f67b4cf8410f0f2818
John Praddo <jhoncitof1ea10@gmail.com>
2023-01-05 20:28:43 -0500

    teerminado

--- ejer04Arreglos.py
+++ ejer04Arreglos.py
@@ -1,3 +1,18 @@
 '''Diseñe un programa que lea un vector y calcule si hay
 un número que sea igual a la suma de los demás elementos
 del vector.'''
+
+import numpy as np
+
+vector = np.array([3,8,5,16])
+print(vector[1])
+if vector[3]== vector[0] +vector[1] +vector[2]:
+    print('LA SUMA' )
+elif vector[2] == vector[0] +vector[1]+vector[3]:
+        print('LA SUMA' )
+elif vector[1]== vector[0]+vector[2]+vector[3]:
+        print('LA SUMA' )
+elif vector[0] == vector[1]+vector[2]+vector[3]:
+        print('LA SUMA' )
+else:
+    print("No hay un numero que sea igual a la suma de los demas elementos ")
