796594102eceda0f5f3128395c33208213218e1a
John Praddo <jhoncitof1ea10@gmail.com>
2023-01-06 11:24:18 -0500

    termianndo con una nueva funcion

--- ejer03Arreglos.py
+++ ejer03Arreglos.py
@@ -5,12 +5,24 @@ por teclado.
 Por ejemplo, si defino un array de tamaño 5 y elijo un
 3 en la función, el array contendrá 3, 6, 9, 12, 15.
 Muéstralos por pantalla usando otra función distinta.'''    
-#definimos los arrays
-tamanArreglo = int(input(u"Ingrese el tamaño del arreglo : "))
-multiplo     = int(input(u"Ingrese el número de múltiplos : "))
+#definimos los arrays 
+'''tamanArreglo = int(input("Ingrese el tamaño del arreglo : "))
+multiplo     = int(input("Ingrese el número de múltiplos : "))
 #creando una lista vacia para almacenar los valores dentro de ella
 A = []
 # par que obvie el cero y que vaye hasta el numero indicado
 for i in range (1,tamanArreglo+1):      
     A.append(i*multiplo)
-print(A)
+print(A)'''
+
+tamanArreglo = int(input("Ingrese el tamaño del arreglo : "))
+#definiendo una funcion
+def rellenar():
+    multiplo     = int(input("Ingrese el número de múltiplos : "))
+    A=[]
+    for i in range (1,tamanArreglo+1):      
+        A.append(i*multiplo)
+    print(A)
+rellenar()
+
+
